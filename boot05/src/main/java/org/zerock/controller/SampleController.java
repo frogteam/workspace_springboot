0c9b7dbdc1de5abddd50f0848f57bcff9eedca16
frogteam <frogteam@naver.com>
2020-08-16 01:55:09 +0900

    200815 Part2 마무리!

--- boot05/src/main/java/org/zerock/controller/SampleController.java
+++ boot05/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,10 @@
 package org.zerock.controller;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,4 +26,102 @@ public class SampleController {
 		
 		model.addAttribute("vo", vo);
 	}
+	
+	@GetMapping("/sample3")
+	public void sample3(Model model) {
+		List<MemberVO> list = new ArrayList<>();
+		
+		for(int i = 0; i < 10; i++) {
+			list.add(new MemberVO(123, "u0" + i, "p0" + i, "홍길동" + i,
+					new Timestamp(System.currentTimeMillis())));
+		}
+		
+		model.addAttribute("list", list);
+		
+	}
+	
+	@GetMapping("/sample4")
+	public void sample4(Model model) {
+
+		List<MemberVO> list = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++) {
+
+			list.add(new MemberVO(i, "u000" + i % 3, "p0000" + i % 3, "홍길동" + i,
+					new Timestamp(System.currentTimeMillis())));
+		}
+
+		model.addAttribute("list", list);
+	}
+	
+	@GetMapping("/sample5")
+	public void sample5(Model model) {
+		String result = "SUCCESS";
+		model.addAttribute("result", result);
+	}
+	
+	@GetMapping("/sample6")
+	public void sample6(Model model) {
+		List<MemberVO> list = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++) {
+			list.add(new MemberVO(i, "u0" + i, "p0" + i, "홍길동" + i,
+					new Timestamp(System.currentTimeMillis())));
+		}
+
+		model.addAttribute("list", list);
+		String result = "SUCCESS";
+		model.addAttribute("result", result);
+	}
+	
+	@GetMapping("/sample7")
+	public void sample7(Model model) {
+		model.addAttribute("now", new Date());
+		model.addAttribute("price", 123456789);
+		model.addAttribute("title", "This is a just sample.");
+		model.addAttribute("options", Arrays.asList("AAAA","BBB","CCC","DDD"));
+	}
+	
+	@GetMapping("/sample8")
+	public void sample8(Model model) {
+		
+	}
+	
+	@GetMapping("/sample/hello")
+	public void hello(){
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
